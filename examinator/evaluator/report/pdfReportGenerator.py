bb868b91e1a03b5fc3ddca5192fd344d06b4bfa1
usman2897 <musman2897@gmail.com>
2018-04-09 23:24:30 +0530

    Added pdf file generation feature to backend

--- examinator/evaluator/report/pdfReportGenerator.py
+++ examinator/evaluator/report/pdfReportGenerator.py
@@ -1,30 +1,21 @@
-from django.template import Template, Context
 import preppy
-from io import StringIO
+import pdfkit
 
 class PdfReport():
     def __init__(self, result):
         self.result = result
 
-    def getReport(self):
-        """We used django template to write the RML, but you could use any other
-        template language of your choice.
-        """
+    def getHtml(self):
         template = preppy.getModule('examinator/evaluator/report/report.prep')
-
         html = template.getOutput({ 'students' : self.result})
-
-        #t = Template(open('examinator/evaluator/report/hello.rml').read())
-        #c = Context(self.result)
-        #rml = t.render(c)
-        #django templates are unicode, and so need to be encoded to utf-8
-        #return rml.encode('utf-8')
         return html
+
+    def getReport(self):
+        html = self.getHtml();
+        pdfkit.from_string(html, 'examinator/evaluator/report/report.pdf')
+        return open('examinator/evaluator/report/report.pdf', 'rb').read()
 
     def getPdf(self):
-        html = self.getReport()
+        return self.getReport()
 
-        #create the pdf
-        #rml2pdf.go(asBytes(rml), outputFileName=buf)
-        #pdfData = buf.getvalue()
-        return html
+    

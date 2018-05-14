a5b3cc42518ee3f29826863eaa464ddc6e197d39
Nikhil Raj <rajnikhil870@gmail.com>
2018-05-14 15:52:30 +0530

    changed the upload location to images

--- examinator/evaluator/models.py
+++ examinator/evaluator/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='document/')
+    document = models.FileField(upload_to='images/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def display_document_content(self):

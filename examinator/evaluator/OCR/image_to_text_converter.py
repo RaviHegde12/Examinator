414b363dedf4e2d10af065799e397f04c58b2733
Nikhil Raj <rajnikhil870@gmail.com>
2018-05-14 15:40:32 +0530

    The master program for converting the image to text
    The program uses Google's Tesseract engine for the conversion. For Django, we're using pytesseract (python wrapper around tesseract).

---
+++ examinator/evaluator/OCR/image_to_text_converter.py
@@ -0,0 +1,23 @@
+import os
+import pytesseract
+from examinator.settings.common import MEDIA_ROOT
+from PIL import Image, ImageEnhance, ImageFilter
+
+
+def ImageToTextConverter(file):
+    path = MEDIA_ROOT +"/images/"+ file
+    img = Image.open(path)
+    img = img.convert('RGB')
+    pix = img.load()
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            if pix[x, y][0] < 102 or pix[x, y][1] < 102 or pix[x, y][2] < 102:
+                pix[x, y] = (0, 0, 0, 255)
+            else:
+                pix[x, y] = (255, 255, 255, 255)
+    img.save(MEDIA_ROOT+'/images/processed_' + file)
+    text = pytesseract.image_to_string(Image.open(MEDIA_ROOT+'/images/processed_' + file))
+    os.remove(MEDIA_ROOT+'/images/processed_' + file)
+    text_ouput = open(MEDIA_ROOT+"/images/text_" + file + ".txt", "w")
+    text_ouput.write(text)
+    text_ouput.close()

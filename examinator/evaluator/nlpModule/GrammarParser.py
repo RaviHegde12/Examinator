5265ef3cd2bfb851e27ea2987dc6789f6c251c6f
usman2897 <musman2897@gmail.com>
2018-06-13 05:37:31 +0530

    Added TestCase for GrammarParsing with passage and queries

--- examinator/evaluator/nlpModule/GrammarParser.py
+++ examinator/evaluator/nlpModule/GrammarParser.py
@@ -1,9 +1,11 @@
 import nltk
 
+DIRECTORY = "steve_life"
+
 class GrammarParser():
 
     def  __init__(self, grammarFilePath):
-        self.grammar = nltk.data.load('file:./testcase/englishGrammar.cfg')
+        self.grammar = nltk.data.load('file:' + grammarFilePath)
         self.parser = nltk.parse.chart.BottomUpLeftCornerChartParser(self.grammar)
 
     def parse(self, inp):
@@ -11,13 +13,33 @@ class GrammarParser():
 
 
 def ie_preprocess(document):
+    lem = nltk.WordNetLemmatizer()
     sentences = nltk.sent_tokenize(document)
     tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
     sentences = [tokenizer.tokenize(sent) for sent in sentences]
-    return sentences
+    lem_sents = []
+    for sent in sentences:
+        wordList = []
+        for word in sent:
+            wordList.append(lem.lemmatize(word))
+        lem_sents.append(wordList)
+    stopwords = ['his', 'called', 'about', 'very', 'again']
+    # content = [word for word in sent for sent in sentences if word.lower() not in stopwords]
+    content = []
+    for sent in lem_sents:
+        wordList = []
+        for taggedWord in sent:
+            if taggedWord.lower() not in stopwords:
+                wordList.append(taggedWord.lower())
+        content.append(wordList)
+    return content
 
-inp = open("./testcase/testCasePollution.txt", 'r+').read()
-parser = GrammarParser("./testcase/englishGrammar.cfg")
+
+inp = open("./testcase/" + DIRECTORY + "/passage.txt", 'r+').read()
+parser = GrammarParser("./testcase/" + DIRECTORY + "/grammar.cfg")
+
+parseTree = []
+# print(ie_preprocess(inp))
 for sent in ie_preprocess(inp):
     for tree in parser.parse(sent):
         print(tree)
--- examinator/evaluator/nlpModule/ReadWrite.py
+++ examinator/evaluator/nlpModule/ReadWrite.py
@@ -1,5 +1,7 @@
 import nltk
 
+DIRECTORY = "steve_life"
+
 class ReadWrite():
     def __init__(self, readFilePath, writeFilePath):
         self.readFile = open(readFilePath, 'r+')
@@ -22,11 +24,31 @@ class ReadWrite():
             self.writeFile.write("{0} -> {1}\n".format(key, strRepCfg[:len(strRepCfg) - 3]))
 
 def ie_preprocess(document):
+    lem = nltk.WordNetLemmatizer()
     sentences = nltk.sent_tokenize(document)
     tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
     sentences = [tokenizer.tokenize(sent) for sent in sentences]
-    sentences = [nltk.pos_tag(sent) for sent in sentences]
-    return sentences
+    # lem_sents = [lem.lemmatize(word) for word in sent for sent in sentences]
+    lem_sents = []
+    for sent in sentences:
+        wordList = []
+        for word in sent:
+            wordList.append(lem.lemmatize(word))
+        lem_sents.append(wordList)
+    sentences = [nltk.pos_tag(sent) for sent in lem_sents]
+    stopwords = ['his', 'called', 'about', 'very', 'again']
+    content = []
+    for sent in sentences:
+        wordList = []
+        for taggedWord in sent:
+            if taggedWord[0].lower() not in stopwords and taggedWord[1].lower() not in stopwords:
+                wordList.append(taggedWord)
+        content.append(wordList)
+    return content
 
-f = ReadWrite("./testcase/testCasePollution.txt", "./testcase/englishGrammar.cfg")
-f.writeToFile(ie_preprocess(f.readFromFile()))
+# f = ReadWrite("./testcase/testCasePollution.txt", "./testcase/englishGrammar.cfg")
+# f.writeToFile(ie_preprocess(f.readFromFile()))
+
+f = ReadWrite("./testcase/" + DIRECTORY + "/query.txt", "./testcase/" + DIRECTORY + "/q_grammar.cfg")
+# f.writeToFile(ie_preprocess(f.readFromFile()))
+print(ie_preprocess(f.readFromFile()))

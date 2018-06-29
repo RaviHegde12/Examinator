ccc1d8e3ad3d4bbc9f4de98a10e66fab261431b0
Ravi S Hegde <ravi.hegde@hashedin.com>
2018-06-29 23:11:11 +0530

    Added functionality to type in answer and blueprint and get result back in homepage itself

--- examinator/static/js/main.js
+++ examinator/static/js/main.js
@@ -1,32 +1,3 @@
-function generate()
-{
-    var answer = document.getElementById("answer-area").value;
-    var blueprint = document.getElementById("blueprint-area").value;
-    if(answer === '' || blueprint === ''){
-        alert("Please enter the answer and blueprint!!!")
-    }
-    else {
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: '/generate/',
-            data:{
-                'answer':answer,
-                'blueprint':blueprint,
-                csrfmiddlewaretoken: '{{ csrf_token }}'
-            },
-            success: function(response) {
-                console.log(response.result)
-                let report = document.getElementById("result");
-                report.innerHTML = response.result;
-            },
-            error: function(result) {
-                console.log(result)
-            }
-        })
-    }
-}
-
 function download_answer()
 {
     var answer = document.getElementById("answer-area").value;
